04afbe8724b61edca2606d6318ae17b8573777d4
raphael-bres <62878271+raphael-bres@users.noreply.github.com>
2020-06-04 09:20:52 +0200

    début bloc ajout de données
    Clic sur la carte pour sélectionner 1 point.
    Formulaire mis à jour
    Les données attributaires vont dans la BDD, rien de fait pour la géom

--- app/forms.py
+++ app/forms.py
@@ -7,8 +7,8 @@ from shapely.geometry import point,polygon,linestring,asShape
 from geoalchemy2.shape import from_shape
 
 class LoginForm(FlaskForm):
-    date_attaque = DateField('date_attaque')
-    date_constat = DateField('date_constat')
+    date_attaque = DateField('date_attaque', format='%d/%m/%Y')
+    date_constat = DateField('date_constat', format='%d/%m/%Y')
     nom_agent1 = StringField('nom_agent1')
     nom_agent2 = StringField('nom_agent2')
     proprietaire = StringField('proprietaire')
@@ -17,22 +17,3 @@ class LoginForm(FlaskForm):
     nb_victimes_blesse = IntegerField('nb_victimes_blesse')
     statut = StringField('statut')
     submit = SubmitField('Add to database')
-
-    def validate_statut(self, statut):
-        constats = Constats.query.filter_by(statut=statut.data).first()
-        if constat is not None:
-            raise ValidationError('Please use a different situation.')
-    
-    def validate_nb_victimes_mort(self, nb_victimes_mort):
-        constats = Constats.query.filter_by(nb_victimes_mort=nb_victimes_mort.data).first()
-        if constats is not None:
-            raise ValidationError('Please use a different email address.')
-                
-    def validate_dateAttaque(self, date_attaque):
-        constats = Constats.query.filter_by(date_attaque=date_attaque.data).first()
-        if constats is not None:
-            raise ValidationError('Please use a different name address.')
-    def validate_date(self, date_constat):
-        constats = Constats.query.filter_by(date_constat=date_constat.data).first()
-        if constats is not None:
-            raise ValidationError('Please use a different name address.')    
--- app/routes.py
+++ app/routes.py
@@ -27,30 +27,24 @@ with app.app_context():
 @app.route("/")
 @app.route("/map")
 def map():
-    data=DB.session.query(Constats).all()
-    dataGeom = DB.session.query(func.ST_AsGeoJson(func.ST_Transform(Constats.the_geom_point,4326))).all()
+    dataGeom = DB.session.query(Constats,func.ST_AsGeoJson(func.ST_Transform(Constats.the_geom_point,4326))).all()
     cnsts=[]
-    #print(dataGeom[0])
-    #print(data)
-    cpt=0
-    for i in data:
-        #print(wkt)
-        #print(dataGeom[cpt])
-        geojson=dataGeom[cpt]
-        cpt=cpt+1
+    for d in dataGeom:
+        geojson=geojson=json.loads(d[1])
+        print(geojson)
         dico={}
         dico['geometry']=geojson
         dico['properties']={}
-        dico['properties']['id_constat']=i.id_constat
-        dico['properties']['date_attaque']=i.date_attaque
-        dico['properties']['date_constat']=i.date_constat
-        dico['properties']['nom_agent1']=i.nom_agent1
-        dico['properties']['nom_agent2']=i.nom_agent2
-        dico['properties']['proprietaire']=i.proprietaire
-        dico['properties']['type_animaux']=i.type_animaux
-        dico['properties']['nb_victimes_mort']=i.nb_victimes_mort
-        dico['properties']['nb_victimes_blesse']=i.nb_victimes_blesse
-        dico['properties']['statut']=i.statut
+        dico['properties']['id_constat']=d[0].id_constat
+        dico['properties']['date_attaque']=d[0].date_attaque
+        dico['properties']['date_constat']=d[0].date_constat
+        dico['properties']['nom_agent1']=d[0].nom_agent1
+        dico['properties']['nom_agent2']=d[0].nom_agent2
+        dico['properties']['proprietaire']=d[0].proprietaire
+        dico['properties']['type_animaux']=d[0].type_animaux
+        dico['properties']['nb_victimes_mort']=d[0].nb_victimes_mort
+        dico['properties']['nb_victimes_blesse']=d[0].nb_victimes_blesse
+        dico['properties']['statut']=d[0].statut
         cnsts.append(dico)        
     print(cnsts)
     return render_template('map.html', title='Map', Constats=cnsts)
@@ -63,11 +57,15 @@ def add():
         data = request.form
         constats = Constats(
             date_attaque=data['date_attaque'],
+            date_constat=data['date_constat'],
+            nom_agent1=data['nom_agent1'],
+            nom_agent2=data['nom_agent2'],
+            proprietaire=data['proprietaire'],
+            type_animaux=data['type_animaux'],
             nb_victimes_mort=data['nb_victimes_mort'],
-            moment=data['moment'],
-            chien=data['chien'],
-            berger=data['berger'],
-            valide=data['valide'],
+            nb_victimes_blesse=data['nb_victimes_blesse'],
+            statut=data['statut']
+            #geom : a convertir en wkb et en 2154
         )
         DB.session.add(constats)
         DB.session.commit()
