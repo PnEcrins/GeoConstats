868620263c55e93a358f09689970b4a835925e87
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2022-05-30 15:12:17 +0200

    pass db and ma in env var

--- app/env.py
+++ app/env.py
@@ -1,5 +1,8 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
+os.environ["FLASK_SQLALCHEMY_DB"] = "app.env.DB"
 DB = SQLAlchemy()
+os.environ["FLASK_MARSHMALLOW"] = "app.env.MA"
 MA = Marshmallow()
